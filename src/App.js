6b1b77d5ae8c7aadf1ce139d4625924fce27c6d3
Trictonicmp <code_shadow@hotmail.com>
2022-09-15 11:42:28 -0500

    Add routes to App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      Set up
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/stadium/:id" element={<Stadium />} />
+        <Route path="/my-reservations" element={<MyReservations />} />
+        <Route path="/delete" element={<DeleteStadiums />} />
+      </Routes>
+    </Router>
   );
 }
 
