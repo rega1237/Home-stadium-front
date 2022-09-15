5463beba82334e3a5980ceeb8794e11aaec95fa4
Trictonicmp <code_shadow@hotmail.com>
2022-09-15 16:41:29 -0500

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import {
   Routes,
 } from 'react-router-dom';
 
+import './css/reset.css';
+import './css/page-layout.css';
+
 import Home from './pages/Home';
-import Login from './components/Login';
-import Stadium from './components/Stadium';
-import MyReservations from './components/MyReservations';
-import DeleteStadiums from './components/DeleteStadiums';
-import Navbar from './components/Navbar';
+// import Login from './components/Login';
+// import Stadium from './components/Stadium';
+// import MyReservations from './components/MyReservations';
+// import DeleteStadiums from './components/DeleteStadiums';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/stadium/:id" element={<Stadium />} />
-          <Route path="/my-reservations" element={<MyReservations />} />
-          <Route path="/delete" element={<DeleteStadiums />} /> */}
-        </Routes>
+        <div className="page-body">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/login" element={<Login />} />
+            <Route path="/stadium/:id" element={<Stadium />} />
+            <Route path="/my-reservations" element={<MyReservations />} />
+            <Route path="/delete" element={<DeleteStadiums />} /> */}
+          </Routes>
+        </div>
       </div>
     </Router>
   );
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <Link to="/" className="logo">
+      <img src="https://i.imgur.com/7I9Was5.png" alt="logo" />
+    </Link>
+    <ul className="links">
+      <li className="active">
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">My Reservations</Link>
+      </li>
+      <li>
+        <Link to="/stadiums">Manage Stadiums</Link>
+      </li>
+      <li>
+        <Link to="/logout">Logout</Link>
+      </li>
+    </ul>
+    <div className="social-media">
+      <ul>
+        <li>
+          <i className="fa fa-facebook-square" />
+        </li>
+        <li>
+          <i className="fa fa-instagram" />
+        </li>
+        <li>
+          <i className="fa fa-twitter" />
+        </li>
+      </ul>
+      <p> All rights reserved 2022 </p>
+    </div>
+  </nav>
+);
+
+export default Navbar;
