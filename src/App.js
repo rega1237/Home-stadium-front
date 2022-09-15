f673e0474c9daa14da1eb23ccd6660d557470f5b
arturo <ahpareja@gmail.com>
2022-09-15 12:55:32 -0500

    add dummy api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
   Routes,
 } from 'react-router-dom';
 
-import Home from './components/Home';
+import Home from './pages/Home';
 import Login from './components/Login';
 import Stadium from './components/Stadium';
 import MyReservations from './components/MyReservations';
@@ -15,10 +15,10 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} />
         <Route path="/stadium/:id" element={<Stadium />} />
         <Route path="/my-reservations" element={<MyReservations />} />
-        <Route path="/delete" element={<DeleteStadiums />} />
+        <Route path="/delete" element={<DeleteStadiums />} /> */}
       </Routes>
     </Router>
   );
