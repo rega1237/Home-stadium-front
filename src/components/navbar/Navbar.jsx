38e1ccbd649e31db2eb00775919b5f3d87743564
Trictonicmp <code_shadow@hotmail.com>
2022-09-16 11:05:16 -0500

    Add toggle menu button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,47 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
-const Navbar = () => (
-  <>
-    <button type="button" className="menu-button">
-      <span />
-      <span />
-    </button>
-    <nav className="navbar">
-      <Link to="/" className="logo">
-        <img src="https://i.imgur.com/7I9Was5.png" alt="logo" />
-      </Link>
-      <ul className="links">
-        <li className="active">
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">My Reservations</Link>
-        </li>
-        <li>
-          <Link to="/stadiums">Manage Stadiums</Link>
-        </li>
-        <li>
-          <Link to="/logout">Logout</Link>
-        </li>
-      </ul>
-      <div className="social-media">
-        <ul>
-          <li>
-            <i className="fa fa-facebook-square" />
+const Navbar = () => {
+  const [isMenuActive, setMenuState] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuState(!isMenuActive);
+  };
+
+  return (
+    <>
+      <button type="button" className="menu-button" onClick={toggleMenu}>
+        <span />
+        <span />
+      </button>
+      <nav className={isMenuActive ? 'navbar hidden' : 'navbar'}>
+        <Link to="/" className="logo">
+          <img src="https://i.imgur.com/7I9Was5.png" alt="logo" />
+        </Link>
+        <ul className="links">
+          <li className="active">
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <i className="fa fa-instagram" />
+            <Link to="/about">My Reservations</Link>
           </li>
           <li>
-            <i className="fa fa-twitter" />
+            <Link to="/stadiums">Manage Stadiums</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
           </li>
         </ul>
-        <p> All rights reserved 2022 </p>
-      </div>
-    </nav>
-  </>
-);
+        <div className="social-media">
+          <ul>
+            <li>
+              <i className="fa fa-facebook-square" />
+            </li>
+            <li>
+              <i className="fa fa-instagram" />
+            </li>
+            <li>
+              <i className="fa fa-twitter" />
+            </li>
+          </ul>
+          <p> All rights reserved 2022 </p>
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
