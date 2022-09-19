d62c235c97eb2b27c9a9d8a8d6a23703051864d9
arturo <ahpareja@gmail.com>
2022-09-19 15:24:02 -0500

    add carousel

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,64 +1,13 @@
 import React, { useEffect } from "react";
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchItems } from '../../redux/homereducer/homereducer';
+import { Link } from "react-router-dom";
 import "./slider.css";
-
-const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  initialSlide: 0,
-  responsive: [
-    {
-      breakpoint: 2560,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 1444,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        initialSlide: 2,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 425,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-};
+import 'swiper/css';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Sliders = () => {
   const dispatch = useDispatch();
@@ -68,25 +17,33 @@ const Sliders = () => {
     dispatch(fetchItems());
   }, [dispatch]);
 
-  
   return (
-    <div className="slider">
-      <Slider {...settings} className="master">
-        {items.map((item) => (
-          <div key={item.id} className="cards">
-            <div className="card-top">
-              <img src={item.img} alt={item.title} className="slider-pic" />
+    <Swiper
+      spaceBetween={20}
+      slidesPerView={3}
+      navigation={true}
+      loop={true}
+      modules={[Navigation, Pagination]} 
+      className="mySwiper"
+      pagination={{
+        clickable: true,
+      }}
+    >
+      {items.map((item) => (
+        <SwiperSlide key={item.id}>
+          <div className="card">
+            <Link to="/stadium/:id" >
+              <img src={item.img} alt="stadium" className="slider-pic" />
+            </Link>
+            <div className="card-body">
+              <h3>Name: {item.name}</h3>
+              <p>Seats: {item.seats}</p>
             </div>
-            <div className="card-bottom">
-              <h3>{item.name}</h3>
-              <p>{item.country}</p>
-              <p>{item.seats}</p>
-            </div> 
           </div>
-        ))}
-      </Slider> 
-    </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
-}
+};
 
 export default Sliders;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Sliders from '../../components/slider/slider';
+import './home.css';
 
 
 const Home = () => {
  
   return (
-    <div className='slider-container'>
-      <Sliders />
+    <div className="home">
+        <div className="title">
+          <h1>STADIUMS</h1>
+          <p>Please select one Stadium reservation</p>
+        </div>
+      <div className='slider-container'>
+        <Sliders />
+      </div>
     </div>
   );
   
