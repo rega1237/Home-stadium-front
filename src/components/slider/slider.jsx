0e7771936c7cd4a1fce765ea849441d1a764b91c
Trictonicmp <code_shadow@hotmail.com>
2022-09-22 14:34:54 -0500

    Fix linter errors

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -28,6 +28,25 @@ const Sliders = () => {
       pagination={{
         clickable: true,
       }}
+      breakpoints={{
+        '@0.00': {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        '@0.75': {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        '@1.00': {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        '@1.50': {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      }}
+
     >
       {items.map((item) => (
         <SwiperSlide key={item.id}>
