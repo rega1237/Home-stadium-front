067ded4440ef30b44b439b24e064de3909942c0c
Trictonicmp <code_shadow@hotmail.com>
2022-09-22 16:07:03 -0500

    Change stadium page component to match backend

--- src/pages/Stadium/Stadium.jsx
+++ src/pages/Stadium/Stadium.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { fetchStadium } from '../../redux/stadiumReducer/StadiumReducer';
 import StadiumImage from '../../components/stadium_image/StadiumImage';
 import StadiumInfo from '../../components/stadium_info/StadiumInfo';
+import { redirectIfNotLogged } from '../../helpers/helpers';
 import './stadium.css';
 
 const Stadium = () => {
   const dispatch = useDispatch();
   const { stadium } = useSelector((state) => state.stadium);
+  const { user } = useSelector((state) => state.users);
+  const { id } = useParams();
 
   useEffect(() => {
-    dispatch(fetchStadium());
+    redirectIfNotLogged(user);
+    dispatch(fetchStadium(user.token, id));
   }, [dispatch]);
 
   return (
     <section className="stadium-wrapper">
-      <StadiumImage src={stadium?.img} alt={stadium?.name} />
+      <StadiumImage src={stadium?.photo} alt={stadium?.name} />
       <StadiumInfo stadium={stadium} />
     </section>
   );
